b3548faae28d93de268eb571f197236cca1452ba
collins <collinskipkoech95@gmail.com>
2021-11-17 20:12:31 +0300

    added header web element

---
+++ src/test/java/com/cheatSheat/pages/NextBaseLogin.java
@@ -0,0 +1,15 @@
+package com.cheatSheat.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NextBaseLogin {
+
+    @FindBy(xpath = "//div[.='Authorization']")
+    public WebElement headerElement;
+
+
+
+
+
+}
