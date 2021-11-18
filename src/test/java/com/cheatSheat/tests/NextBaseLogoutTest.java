09a1cb64d2da9dd29d8935f94b3f61f2ff070822
collins <collinskipkoech95@gmail.com>
2021-11-18 10:08:33 +0300

    created logout and login instances, then get the url,login and logout using defined instance methods

--- src/test/java/com/cheatSheat/tests/NextBaseLogoutTest.java
+++ src/test/java/com/cheatSheat/tests/NextBaseLogoutTest.java
@@ -1,5 +1,9 @@
 package com.cheatSheat.tests;
 
+import com.cheatSheat.pages.NextBaseLogin;
+import com.cheatSheat.pages.NextBaseLogout;
+import com.cheatSheat.utility.BrowserUtil;
+import com.cheatSheat.utility.ConfigReader;
 import com.cheatSheat.utility.TestBase;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +11,20 @@ public class NextBaseLogoutTest extends TestBase {
 
     @Test
     public void testLogout(){
+
+        NextBaseLogout nextBaseLogout = new NextBaseLogout();
+        NextBaseLogin nextBaseLogin = new NextBaseLogin();
+
+        String username = ConfigReader.read("username");
+        String password = ConfigReader.read("password");
+
+        nextBaseLogout.goTo();
+        nextBaseLogin.login(username,password);
+
+        BrowserUtil.waitFor(3);
+
+        nextBaseLogout.logout();
+
 
 
     }
