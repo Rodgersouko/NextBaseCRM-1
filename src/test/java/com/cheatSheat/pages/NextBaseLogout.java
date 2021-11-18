85e7d7703a1cb2e9e4f9a4966d5f3ef0f55b3e07
collins <collinskipkoech95@gmail.com>
2021-11-18 09:51:31 +0300

    added input web element from the home page

---
+++ src/test/java/com/cheatSheat/pages/NextBaseLogout.java
@@ -0,0 +1,10 @@
+package com.cheatSheat.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NextBaseLogout {
+
+    @FindBy(xpath = "//input[@name='q']")
+    public WebElement inputBox;
+}
